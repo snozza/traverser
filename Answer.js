587337ddcf592157fcc8da9e4a3496d57803552f
Andrew Snead <andrewcsnead@gmail.com>
2015-02-21 17:22:38 +0000

    working

--- Answer.js
+++ Answer.js
@@ -1,59 +1,36 @@
 function Traverser() {
 };
 
-Traverser.prototype.options = function(elements, selector) {
-  var type = "elem";
-  if(selector[0] === '#' || selector[0] ==='.') type = selector[0];
-  this.finders = {
-    ".": this.classFinder(elements, selector.slice(1)),
-    "#": this.idFinder(elements, selector.slice(1)),
-    "elem": this.elFinder(elements, selector)
-  }[type];
+Traverser.prototype.parser = function(elems, select) {
+  if(select[0].match(/[^\.#]/)) return this.elFinder(elems, select.split(/\.|#/));
+  return this.idClassFinder(elems, select.slice(1));
 };
 
-Traverser.prototype.elFinder = function(elements, selector) {
-  var selector = selector.split('.');
-  var items = document.getElementsByTagName(selector[0]);
-  if(selector[1] && items.length > 1) 
-    return this.classFinder(elements, selector[1].split('#')[0], items)
-  this.addEl(elements, items);
+Traverser.prototype.elFinder = function(elems, select) {
+  if(select.length === 1)
+    return this.addEl(elems, document.getElementsByTagName(select[0]));
+  this.findFilter(elems, select, document.getElementsByTagName(select[0]));
 };
 
-Traverser.prototype.classFinder = function(elements, selector, filter) {
-  var items = [];
-  if(filter) {
-    for(var i = 0; i < filter.length; i++)
-      if(filter[i].getAttribute('class')) {
-        if(filter[i].classList.contains(selector)) {
-          elements.push(filter[i]);
-      }
-    }
-  }
-  else {
-    items = document.getElementsByClassName(selector); 
-    this.addEl(elements, items, filter);
-  }
+Traverser.prototype.findFilter = function(elems, select, elemList) {
+  for (var i=0; i < elemList.length; i++)
+    if(elemList[i].classList.contains(select[1]) || elemList[i].id == select[1])
+      this.addEl(elems, [elemList[i]]);     
 };
 
-Traverser.prototype.idFinder = function(elements, selector) {
-  var item = document.getElementById(selector);
-  if(item && item.id) console.log(item.id);
-  if(item) elements.push(item);
+Traverser.prototype.idClassFinder = function(elems, select) {
+  var item = [document.getElementById(select)] 
+  if(!item[0]) item = document.getElementsByClassName(select);
+  this.addEl(elems, item);
 };
 
-Traverser.prototype.addEl = function(elements, selectors, filter) {
-  var filter = filter || elements;
-  if(selectors)
-  for(var i = 0; i < selectors.length; i++)
-    // if(filter.indexOf(selectors[i] > -1)) 
-      elements.push(selectors[i]);
+Traverser.prototype.addEl = function(elems, selects) {
+  for(var i = 0; i < selects.length; i++)
+    elems.push(selects[i]);
 };
 
-
-var traverser = new Traverser();
-
-var $ = function (selector) {
+var $ = function (select) {
   var elements = [];
-  traverser.options(elements, selector);
+  new Traverser().parser(elements, select);
   return elements;
 }
